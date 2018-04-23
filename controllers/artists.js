5fac9fad9a896281d1ffc9545463fb86d5abae5e
Nathan Fleurisma <nfleurisma@gmail.com>
2018-04-23 14:08:50 -0400

    first parts

---
+++ controllers/artists.js
@@ -0,0 +1,34 @@
+const artistModel = require('../models/artist');
+
+const readAll = (request, response) => {
+  const artists = artistModel.readAll();
+  response.send(artists);
+}
+
+const readIndividual = (request, response) => {
+  const artist = artistModel.readIndividual(request.params.id);
+  response.send(artist);
+}
+
+const create = (request, response) => {
+  const artist = artistModel.create(request.body);
+  response.send(artist);
+}
+
+const update = (request, response) => {
+  const artist = artistModel.update(request.params.id, request.body);
+  response.send(artist);
+}
+
+const destroy = (request, response) => {
+  const artist = artistModel.destroy(request.params.id);
+  response.send(artist);
+}
+
+module.exports = {
+  readAll,
+  readIndividual,
+  create,
+  update,
+  destroy
+};
